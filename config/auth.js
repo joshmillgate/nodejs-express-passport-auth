510baf7f9ca1ed846dd0a0d6452780d07283887b
joshmillgate <hello@joshmillgate.co.uk>
2020-02-10 23:00:12 +0000

    First Commit

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+    ensureAuthenticated: function (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        req.flash('error_msg', 'Please log in to view this page');
+        res.redirect('/users/login');
+    },
+    forwardAuthenticated: function (req, res, next) {
+        if (!req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect('/dashboard');
+    }
+};
